1e022af5398af5f104012298b654d031b05d29c0
b0ndurant <martin.aurelien01@gmail.com>
2019-04-29 01:35:09 +0200

    fix bug

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git commit -m 'deploy'
 
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:b0ndurant/Monster-slayer.git master:gh-pages
+git push -f git@github.com:b0ndurant/monster-slayer.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Monster-slayer/'
+    ? '/monster-slayer/'
     : '/'
 }
 
