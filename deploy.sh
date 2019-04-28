a518853d1d869c6fca525bb459dc0f1dd7721d6b
b0ndurant <martin.aurelien01@gmail.com>
2019-04-29 00:49:23 +0200

    create deploy.sh and vue.config.js

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:b0ndurant/Monster-slayer.git master:gh-pages
+
+cd -
